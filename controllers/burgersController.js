c7ab8e12c4d51e35cd908fcfb9674bec57032f10
Tee <teecovington@gmail.com>
2018-08-18 13:25:48 -0400

    update

---
+++ controllers/burgersController.js
@@ -0,0 +1,43 @@
+var express = require("express");
+
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+ r
+  burger.all(function(burgerData) {
+    res.render("index", { burger_data: burgerData });
+  });
+});
+
+
+router.post("/burgers/create", function(req, res) {
+  burger.create(req.body.burger_name, function(result) {
+    console.log(result);
+    res.redirect("/");
+  });
+});
+router.put("/burgers/update", function(req, res) {
+  burger.update(req.body.burger_id, function(result) {
+
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+
+router.delete("/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("Deleting", condition);
+
+    burger.delete(condition, function() {
+        res.redirect("/");
+    });
+});
+
+
+module.exports = router;
